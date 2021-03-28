1b4c7070caed914ec0f156b36bf6a6f7f53e89d5
Lukáš Varga <lukas.varga128@gmail.com>
2021-03-28 23:00:22 +0200

    Finishing the first introductory lesson of programming in C++.

---
+++ FirstApp/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>             //iostream header
+#include <cstdlib>              //cstdlib header
+
+using namespace std;            //namespace
+
+int main(int argc, char *argv[])//main function of program
+{
+    cout << "Lukas Varga" << endl;  //write to console
+    cout << "Weiherweg 6/1.OG";     //write to console
+    cout << "Nr. 3.112" << "\n";    //write to console \n
+    cout << "93051 Regensburg";     //write to console
+    cout << endl;                   //blank space
+
+    system("pause");                //wait for pressing key
+    system("cls");                  //clear of console
+    system("pause");                //wait for pressing key
+    return 0;                       //terminate program
+}
---
+++ FourSeasons/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    cout << "Spring" << endl;
+    cout << "Summer" << endl;
+    cout << "Autumn" << endl;
+    cout << "Winter" << endl;
+
+    system("pause");
+    return 0;
+}
